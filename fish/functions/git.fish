9bfb0e27ca8cae5428e4fc773a2b1f9aeee97869
Stephan Rayner <stephan@echosec.net>
2017-07-13 14:00:19 -0700

    Added g.pull and g.push

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,1 +1,10 @@
 alias asdf="clear; pwd; echo; git branch; git status"
+alias g.branch="git branch | grep '*' | tr '*' ' ' | tr -d '[:space:]'"
+
+function g.push
+    git push origin (g.branch)
+end
+
+function g.pull
+    git pull origin (g.branch)
+end
