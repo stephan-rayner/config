a4408d25f6e15c763dbd8595a34a57e1d27eec51
Stephan Rayner <stephan@echosec.net>
2017-07-13 14:46:51 -0700

    Removed make-ignore.sh

--- git/make-ignore.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# MAINTAINER Stephan Rayner <stephan.rayner@gmail.com>
-
-EDITORS=vim,visualstudiocode,sublimetext
-LANGS=python,node
-
-wget "https://www.gitignore.io/api/${EDITORS},${LANGS}"
-mv $EDITORS,$LANGS gitignore
