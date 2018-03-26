fe92c08367ac6e9137641810e0570e3c13b8f39c
Stephan Rayner <srayner@selkirksystems.com>
2018-03-26 11:21:23 -0700

    Adding fetch all functionality to git shortcuts

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -7,13 +7,21 @@ function g.push
     git push origin (g.branch)
 end
 
+
 function g.pull
     git pull origin (g.branch)
 end
+
 
 function g.ignore
     set EDITORS 'vim,visualstudiocode,sublimetext'
     set LANGS $argv[1]
     wget https://www.gitignore.io/api/$EDITORS,$LANGS
     mv $EDITORS,$LANGS ./.gitignore
+end
+
+
+function g.fetch-all
+    bash (dirname (status --current-filename))/scripts/git-fetch-all.sh
+
 end
